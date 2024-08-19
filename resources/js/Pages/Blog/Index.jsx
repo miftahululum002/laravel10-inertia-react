13cfa107878a55f6f1723dd9ba6ce88ebdedc116
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-19 12:17:19 +0700

    blog select

---
+++ resources/js/Pages/Blog/Index.jsx
@@ -0,0 +1,49 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Link, Head } from "@inertiajs/react";
+
+export default function Dashboard({ blogs, auth }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Blog
+                </h2>
+            }
+        >
+            <Head title="Dashboard" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-1">
+                        <table className="table-auto w-full text-sm text-left text-gray-500 dark:text-gray-400 border">
+                            <thead className="text-slate-500">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Title</th>
+                                    <th>Kontent</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {blogs.map((blog, index) => (
+                                    <tr
+                                        key={index}
+                                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                                    >
+                                        <td>{index + 1}</td>
+                                        <td className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                            {blog.title}
+                                        </td>
+                                        <td className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                            {blog.content}
+                                        </td>
+                                        <td className="text-center"></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
