4258591ae2b9d1989a9da1b596f214511a9b794a
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-19 12:21:29 +0700

    ihnore dan title

--- resources/js/Pages/Blog/Index.jsx
+++ resources/js/Pages/Blog/Index.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Link, Head } from "@inertiajs/react";
 
-export default function Dashboard({ blogs, auth }) {
+export default function Blog({ blogs, auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -11,7 +11,7 @@ export default function Dashboard({ blogs, auth }) {
                 </h2>
             }
         >
-            <Head title="Dashboard" />
+            <Head title="Blog" />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-1">
