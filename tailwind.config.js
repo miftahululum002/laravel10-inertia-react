e59d7877bd4717536601aa4a5266068eebb6e062
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-20 19:10:59 +0700

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class",
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
@@ -18,6 +19,7 @@ export default {
             colors: {
                 primary: "#0ea5e9",
                 secondary: "#e7e5e4",
+                dark: "#1f2937",
             },
         },
     },
