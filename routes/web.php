f7f828f61240c24e2310701db0c0660f61874293
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-20 10:31:15 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -38,6 +39,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/blogs', BlogController::class);
     Route::resource('/products', ProductController::class);
+    Route::resource('/contacts', ContactController::class);
 });
 
 require __DIR__ . '/auth.php';
