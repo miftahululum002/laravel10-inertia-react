17102381cc22cfc865ed0a64a2f090f8fedd0c25
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-20 16:51:33 +0700

    oke

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -16,41 +16,42 @@ export default function ForgotPassword({ status }) {
     };
 
     return (
-        <GuestLayout>
+        <>
             <Head title="Forgot Password" />
-
-            <div className="mb-4 text-sm text-gray-600">
-                Forgot your password? No problem. Just let us know your email
-                address and we will email you a password reset link that will
-                allow you to choose a new one.
-            </div>
-
-            {status && (
-                <div className="mb-4 font-medium text-sm text-green-600">
-                    {status}
+            <GuestLayout>
+                <div className="mb-4 text-sm text-gray-600">
+                    Forgot your password? No problem. Just let us know your
+                    email address and we will email you a password reset link
+                    that will allow you to choose a new one.
                 </div>
-            )}
 
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    placeholder="Email"
-                    onChange={(e) => setData("email", e.target.value)}
-                />
+                {status && (
+                    <div className="mb-4 font-medium text-sm text-green-600">
+                        {status}
+                    </div>
+                )}
 
-                <InputError message={errors.email} className="mt-2" />
+                <form onSubmit={submit}>
+                    <TextInput
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={data.email}
+                        className="mt-1 block w-full"
+                        isFocused={true}
+                        placeholder="Email"
+                        onChange={(e) => setData("email", e.target.value)}
+                    />
 
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+                    <InputError message={errors.email} className="mt-2" />
+
+                    <div className="flex items-center justify-end mt-4">
+                        <PrimaryButton className="ms-4" disabled={processing}>
+                            Email Password Reset Link
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </GuestLayout>
+        </>
     );
 }
