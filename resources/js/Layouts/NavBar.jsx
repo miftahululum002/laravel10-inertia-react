7cde60f57fd120ae4712ebe6df8a819f7123343a
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-08-20 16:42:11 +0700

    navbar from data

--- resources/js/Layouts/NavBar.jsx
+++ resources/js/Layouts/NavBar.jsx
@@ -1,32 +1,23 @@
 import NavLink from "@/Components/NavLink";
 
+const links = [
+    { label: "Dashboard", route: "dashboard" },
+    { label: "Blog", route: "blogs.index" },
+    { label: "Produk", route: "products.index" },
+    { label: "Kontak", route: "contacts.index" },
+];
 export default function NavBar() {
     return (
         <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-            <NavLink
-                href={route("dashboard")}
-                active={route().current("dashboard")}
-            >
-                Dashboard
-            </NavLink>
-            <NavLink
-                href={route("blogs.index")}
-                active={route().current("blogs.index")}
-            >
-                Blog
-            </NavLink>
-            <NavLink
-                href={route("products.index")}
-                active={route().current("products.index")}
-            >
-                Produk
-            </NavLink>
-            <NavLink
-                href={route("contacts.index")}
-                active={route().current("contacts.index")}
-            >
-                Kontak
-            </NavLink>
+            {links.map((link, index) => (
+                <NavLink
+                    key={index}
+                    href={route(link.route)}
+                    active={route().current(link.route)}
+                >
+                    {link.label}
+                </NavLink>
+            ))}
         </div>
     );
 }
